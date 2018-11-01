662a6d6eae7d8e1dd16ab206551bc47e0989e5a9
lopatoid <lopatoid@gmail.com>
2018-11-01 20:09:36 +0300

    Update speed-up-ubuntu.sh

--- speed-up-ubuntu.sh
+++ speed-up-ubuntu.sh
@@ -1,60 +1,71 @@
 #!/bin/bash
-# Run ./speed-up-ubuntu.sh --remove-some-packages
 
 
 if [ $EUID != 0 ]; then
-    sudo "$0" "$@"
-    exit $?
+  sudo "$0" "$@"
+  exit $?
 fi
 
 
-# Disable updates
+askFor () {
+  if [[ "$arg1" == '-y' ]]; then
+    return 0
+  else
+		echo "$1""? (y/n)"
+		read x
+		[[ "$x" == "y" ]]
+	fi
+}
+
+
 # https://askubuntu.com/questions/1057458/how-to-remove-ubuntus-automatic-internet-connection-needs/1057463#1057463
-systemctl disable --now apt-daily{,-upgrade}.{timer,service}
-echo -e 'APT::Periodic::Update-Package-Lists "0";\nAPT::Periodic::Unattended-Upgrade "0";' > /etc/apt/apt.conf.d/20auto-upgrades
-
-
-# Put /tmp on tmpfs
-cp -v /usr/share/systemd/tmp.mount /etc/systemd/system/
-systemctl enable tmp.mount
-
-
-#  Mounting partitions with noatime
-if ! grep -q  noatime /etc/fstab; then
-	sed -i 's/ext4 */ext4    noatime,/g' /etc/fstab
+if askFor "Disable updates"; then
+	systemctl disable --now apt-daily{,-upgrade}.{timer,service}
+	echo -e 'APT::Periodic::Update-Package-Lists "0";\nAPT::Periodic::Unattended-Upgrade "0";' > /etc/apt/apt.conf.d/20auto-upgrades
 fi
 
 
-# Disable lvmetad 
-if ! grep -q  /dev/mapper /etc/fstab; then
-	sed -i 's/use_lvmetad = 1/use_lvmetad = 0/g' /etc/lvm/lvm.conf
-	systemctl disable --now lvm2-lvmetad{,socket}
-fi 
+if askFor "Put /tmp on tmpfs"; then
+	cp -v /usr/share/systemd/tmp.mount /etc/systemd/system/
+	systemctl enable tmp.mount
+fi
 
 
-# Disable Spectre, Meltdown, L1TF mitigation. Disable NMI watchdog.
+if askFor "Mount partitions with noatime" && (! grep -q  noatime /etc/fstab); then
+  sed -i 's/ext4 */ext4    noatime,/g' /etc/fstab
+fi
+
+
+if askFor "Disable lvmetad" && (! grep -q  /dev/mapper /etc/fstab); then
+  sed -i 's/use_lvmetad = 1/use_lvmetad = 0/g' /etc/lvm/lvm.conf
+  systemctl disable --now lvm2-lvmetad{,socket}
+fi
+
+
 # grep . /sys/devices/system/cpu/vulnerabilities/*
-sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="pti=off spectre_v2=off l1tf=off nospec_store_bypass_disable no_stf_barrier nmi_watchdog=0"/g' /etc/default/grub
-update-grub
+if askFor "Disable Spectre, Meltdown, L1TF mitigation and NMI watchdog"; then
+  sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="pti=off spectre_v2=off l1tf=off nospec_store_bypass_disable no_stf_barrier nmi_watchdog=0"/g' /etc/default/grub
+  update-grub
+fi
 
 
-# configure vm.swappiness
-echo vm.swappiness=10 > /etc/sysctl.d/60-swappiness.conf
+if askFor "Set vm.swappiness to 10"; then
+  echo vm.swappiness=10 > /etc/sysctl.d/60-swappiness.conf
+fi
 
-# i'm pretty sure nobody uses floppy nowadays
-echo "blacklist floppy" > /etc/modprobe.d/blacklist-floppy.conf
 
-if [ "$1" == "--remove-some-packages" ]; then
+if askFor "Disable  floppy disk drive?"; then
+  echo "blacklist floppy" > /etc/modprobe.d/blacklist-floppy.conf
+fi
+
+
+if askFor "Remove some packages (please see source of this script)"; then
 	apt update
 
 	# optimize VM guests
 	if [ "$(systemd-detect-virt)" = "vmware" ] && [ -z `which vmtoolsd` ]; then
 		apt install -y open-vm-tools
 	fi
-	# if [ "$(systemd-detect-virt)" = "kvm" ]; then
-	# 	apt install -y linux-kvm
-	# fi
-
 
 	# remove really unnecassary (in my case) packages
 	apt purge -y whoopsie libwhoopsie0
@@ -66,4 +77,7 @@ if [ "$1" == "--remove-some-packages" ]; then
 	apt clean
 fi
 
-echo 'Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot
+
+if askFor "reboot"; then
+  /sbin/reboot
+fi
